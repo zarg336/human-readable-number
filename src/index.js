1d6deb634fc3466bdee9f441a2121f0cb02b98bc
zarg336 <70839905+zarg336@users.noreply.github.com>
2020-10-25 13:26:28 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const One = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const Ten = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number === 0) return 'zero';
+
+    const hundreds = Math.floor(number / 100);
+    const tens = number - hundreds * 100;
+    const tenL = Math.floor(tens / 10);
+    const tenR = tens % 10;
+
+    const result = [];
+    if (hundreds > 0) {
+        result.push(`${One[hundreds - 1]} hundred`);
+    }
+
+    if (tenL > 1) {
+        result.push(`${Ten[tenL - 2]}`);
+
+        if (tenR > 0) {
+            result.push(`${One[tenR - 1]}`);
+        }
+    } else {
+        if (One[tens - 1]) result.push(`${One[tens - 1]}`);
+    }
+
+    return result.join(' ');
 }
